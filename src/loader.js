5d06a314d23354acbcc609774217f5665537512c
patrikx3 <alabard@gmail.com>
2017-12-20 16:34:56 +0100

    p3x-robot sunday release 2017-12-20 16:34:56

--- src/loader.js
+++ src/loader.js
@@ -92,7 +92,7 @@ class loader {
 
 This is an open source project. Just code. If you like this code, please add a star in GitHub and you really like, you can donate as well. Thanks you so much!
 
-Given, I have my own server, with dynamic IP address (except IPv6), it could happen that the server for about max 5 minutes can not be reachable for the dynamic DNS or very rarely I backup with Clonzilla the SSD or something with the electricity (too much hoovering or cleaning - but I worked on it, so should not happen again), but for some reason it is not reachable please hang on for 5-30 minutes and it will be back for sure. 
+Given, I have my own server, with dynamic IP address, it could happen that the server for about max 5 minutes can not be reachable for the dynamic DNS or very rarely I backup with Clonzilla the SSD or something with the electricity (too much hoovering or cleaning - but I worked on it, so should not happen again), but for some reason, it is not reachable please hang on for 5-30 minutes and it will be back for sure. 
 
 ### Node Version Requirement 
 \`\`\` 
