e9468ac46ba8952f5b63183c82504cf127e30dde
patrikx3 <alabard@gmail.com>
2017-12-25 00:24:40 +0100

    p3x-robot sunday release 2017-12-25 00:24:40

--- src/loader.js
+++ src/loader.js
@@ -94,6 +94,8 @@ This is an open source project. Just code. If you like this code, please add a s
 
 Given, I have my own server, with dynamic IP address, it could happen that the server for about max 5 minutes can not be reachable for the dynamic DNS or very rarely I backup with Clonzilla the SSD or something with the electricity (too much hoovering or cleaning - but I worked on it, so should not happen again), but for some reason, it is not reachable please hang on for 5-30 minutes and it will be back for sure. 
 
+All my domains (patrikx3.com and corifeus.com) could have errors right now, since I am developing in my free time and you can catch glitches, but usually it is stable (imagine updating everything always, which is weird).
+
 ### Node Version Requirement 
 \`\`\` 
 \${pkg.engines.node} 
