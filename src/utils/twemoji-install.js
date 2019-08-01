7afb56428cd1cabe0f1dabce0defb17d97fe898d
patrikx3 <alabard@gmail.com>
2019-08-01 20:27:01 +0200

    robo8x [chore]   8/1/2019, 8:27:01 PM

--- src/utils/twemoji-install.js
+++ src/utils/twemoji-install.js
@@ -2,14 +2,16 @@ const utils = require('corifeus-utils')
 const fsExtra = require('fs-extra')
 
 const start = async () => {
-    if (await fsExtra.pathExists('node_modules/twemoji/git')) {
-        await fsExtra.remove('node_modules/twemoji/git')
+    if (await fsExtra.pathExists('node_modules/twemoji/twemoji-master')) {
+        await fsExtra.remove('node_modules/twemoji/twemoji-master')
     }
-    await utils.childProcess.exec('git clone https://github.com/twitter/twemoji.git node_modules/twemoji/git --depth 1', true)
+    await utils.childProcess.exec('curl -L https://github.com/twitter/twemoji/archive/master.zip --output node_modules/twemoji/master.zip', true)
+    await utils.childProcess.exec('unzip node_modules/twemoji/master.zip -d node_modules/twemoji/', true)
     if (await fsExtra.pathExists('node_modules/twemoji/2/svg')) {
         await fsExtra.remove('node_modules/twemoji/2/svg')
     }
-    await fsExtra.move('node_modules/twemoji/git/assets/svg', 'node_modules/twemoji/2/svg')
-    await fsExtra.remove('node_modules/twemoji/git')
+    await fsExtra.move('node_modules/twemoji/twemoji-master/assets/svg', 'node_modules/twemoji/2/svg')
+    await fsExtra.remove('node_modules/twemoji/twemoji-master')
+    await fsExtra.remove('node_modules/twemoji/master.zip')
 }
 start()
